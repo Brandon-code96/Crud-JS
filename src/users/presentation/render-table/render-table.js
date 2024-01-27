7517d626c0261f865153f527bbfdc8cfdaa56625
Brandon Ubillus <brandonubillusbarraza@gmail.com>
2024-01-27 13:18:42 -0500

    Informacion de usuario

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from '../../store/users-store';
+import { showModal } from '../render-modal/render-modal';
 import './render-table.css';
 
 
@@ -26,6 +27,18 @@ const createTable = () => {
 
 /**
  * 
+ * @param {MouseEvent} event 
+ */
+const tableSelectListener = (event) => {
+    const element = event.target.closest('.select-user');
+    if( !element) return;
+
+    const id = element.getAttribute('data-id');
+    showModal(id);
+}
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
 export const renderTable = ( element ) => {
@@ -36,6 +49,7 @@ export const renderTable = ( element ) => {
         element.append( table );
 
         // TODO: listeners a la tabla
+        table.addEventListener('click', tableSelectListener)
     }
 
     let tableHTML = '';
@@ -50,7 +64,7 @@ export const renderTable = ( element ) => {
                 <td>
                     <a href="#/" class="select-user" data-id="${ user.id }> Select </a>
                     |
-                    <a href="#/" class="delete-user data-id="${ user.id }> Delete </a>
+                    <a href="#/" class="delete-user" data-id="${ user.id }> Delete </a>
                 </td>
             </tr>
         `
--- src/users/use-cases/save-user.js
+++ src/users/use-cases/save-user.js
@@ -1,3 +1,4 @@
+import { userModelToLocalhost } from "../mappers/user-to-localhost.mapper";
 import { User } from "../models/user"
 
 /**
@@ -5,14 +6,21 @@ import { User } from "../models/user"
  * @param {Like<User>} userLike 
  */
 export const saveUser =async ( userLike ) => {
+    
     const user = new User( userLike );
+
+    if( !user.firstName || !user.lastName ){
+        throw 'First & last name are required';
+    }
+
+    const userToSave = userModelToLocalhost( user );
 
     if( user.id ){
         throw 'No implementada la actualizacion'
         return;
     }
 
-    const updatedUser = await createUser( user );
+    const updatedUser = await createUser( userToSave );
     return updatedUser;
 }
 
